446e4e79a293613f5dbd9b8a567e7de19c480754
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2022-06-25 02:08:02 -0700

    Also need lambda!

--- readPicoDst.C
+++ readPicoDst.C
@@ -59,8 +59,8 @@ void readPicoDst(const Char_t *inputFile="test.list", int jobindex, int run=11, 
 	StKFParticleInterface::instance()->SetSoftTofPidMode();
 	StKFParticleInterface::instance()->SetChiPrimaryCut(10);
 	//Add decays to the reconstruction list
-	//StKFParticleInterface::instance()->AddDecayToReconstructionList( 3122);
-	//StKFParticleInterface::instance()->AddDecayToReconstructionList(-3122);
+	StKFParticleInterface::instance()->AddDecayToReconstructionList( 3122);
+	StKFParticleInterface::instance()->AddDecayToReconstructionList(-3122);
 	StKFParticleInterface::instance()->AddDecayToReconstructionList( 3334);
 	StKFParticleInterface::instance()->AddDecayToReconstructionList(-3334);
 
