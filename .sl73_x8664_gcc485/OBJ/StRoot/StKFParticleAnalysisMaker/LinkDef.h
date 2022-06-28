69ce924e898a584da300b210ff218764323e8119
Xiatong Wu <maxwoo@rcas6014.rcf.bnl.gov>
2022-06-28 13:38:59 -0400

    current

--- .sl73_x8664_gcc485/OBJ/StRoot/StKFParticleAnalysisMaker/LinkDef.h
+++ .sl73_x8664_gcc485/OBJ/StRoot/StKFParticleAnalysisMaker/LinkDef.h
@@ -3,8 +3,10 @@
 #pragma link off all classes;
 #pragma link off all functions;
 #ifdef __CINT__
+#pragma link C++ class MixedBuffer+;
 #pragma link C++ class StKFParticleAnalysisMaker+;
 #pragma link C++ class StKFParticleInterface+;
 #pragma link C++ class StKFParticlePerformanceInterface+;
 #pragma link C++ class StLambdaDecayPair+;
+#pragma link C++ class my_event+;
 #endif
