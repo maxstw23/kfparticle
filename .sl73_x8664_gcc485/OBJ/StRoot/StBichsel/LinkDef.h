92adc7a5a9516e42f5db3a82096cbaf589c93ae0
Xiatong Wu <maxwoo@rcas6015.rcf.bnl.gov>
2022-08-21 19:21:09 -0400

    current

---
+++ .sl73_x8664_gcc485/OBJ/StRoot/StBichsel/LinkDef.h
@@ -0,0 +1,10 @@
+/* This was generated for version 'TFG21g' */
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ enum Bichsel::EParTypes;
+#ifdef __CINT__
+#pragma link C++ class Bichsel+;
+#pragma link C++ class StdEdxModel+;
+#pragma link C++ class dEdxParameterization+;
+#endif
---
+++ combine2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd output
+mkdir -p temp1
+mkdir -p temp2
+cp ../hadd2.pl ./temp1/
+cp ../hadd2.pl ./temp2/
+
+cp KFParticle*00.root ./temp1/
+cd temp1
+perl hadd2.pl
+rm KFParticle*
+perl hadd2.pl
+cd ../
+
+# cp output_* ./temp2/
+# cd temp2
+# perl hadd2.pl
+# rm output_*
+# perl hadd2.pl
