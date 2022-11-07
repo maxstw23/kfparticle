74ecdf0d104aedbb803f7e676fffcdb29191d787
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2022-11-07 00:33:42 -0800

    Update StKFParticleAnalysisMaker.h

--- StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.h
+++ StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.h
@@ -96,8 +96,8 @@ private:
 	static const float OmegabarMassSigma[7];
 
 	// for event mixing
-	std::vector<float> mix_px, mix_py, mix_pz;
-	std::vector<int> mix_charge;
+	std::vector<float> *mix_px, *mix_py, *mix_pz;
+	std::vector<int> *mix_charge;
 	int mix_evt_id, mix_run_id;
 	bool PerformMixing;
 	bool StoringTree;
