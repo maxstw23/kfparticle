6f436739a0115832151df4253fd337924ad5efb9
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2023-09-10 00:37:14 -0500

    new combine_EPD

--- combine_EPD.sh
+++ combine_EPD.sh
@@ -6,10 +6,10 @@ mkdir -p temp2
 cp ../hadd2.pl ./temp1/
 cp ../hadd2.pl ./temp2/
 
-cp cent_9_EPD_CorrectionOutput_* ./temp1/
+cp cent_EPD_CorrectionOutput_* ./temp1/
 cd temp1
 perl hadd2.pl
-rm cent_9_EPD_CorrectionOutput_*
+rm cent_EPD_CorrectionOutput_*
 perl hadd2.pl
 cd ../
 
