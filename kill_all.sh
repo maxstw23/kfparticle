02cdee4155b5d286c227ad38f6eff4eb2b2dbf18
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2025-02-26 09:54:53 -0800

    Update kill_all.sh

--- kill_all.sh
+++ kill_all.sh
@@ -1,1 +1,1 @@
-python -c 'from RCFNavigator import LongKiller; LongKiller(0,0).kill_bad_job()'
+/star/u/maxwoo/python/Python-3.10.4/python -c 'from RCFNavigator import LongKiller; LongKiller(0,0).kill_bad_job()'
