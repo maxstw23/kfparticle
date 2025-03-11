5cd63213eadc9eaafe42f3886281fe0c44fa3f44
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2025-03-11 14:40:52 -0700

    Update check_missing_files.py

--- check_missing_files.py
+++ check_missing_files.py
@@ -18,15 +18,38 @@ def main():
 			if int(index_str) not in output_index:
 				# print(int(index_str))
 				missing_index.append(int(index_str))
-	
+
 	print(len(missing_index))	
 	missing_index_str = ''
 	for index in missing_index:
 		missing_index_str += f'{index},'
 	missing_index_str = missing_index_str[:-1]
-	with open('resubmit.sh', 'w') as f:
-		f.write('#!/bin/bash\n')
-		f.write(f'star-submit-beta -r {missing_index_str} *.session.xml')	
+
+	# write condor file instead
+	# get jobid, which should be in the format of 'sched5FB88B1A8CDB427602BCD52B2C699D47'
+	jobid = filename.split('_')[0]	
+	# gather pwd
+	pwd = os.environ['PWD']
+	with open('resubmit.condor', 'w') as f:
+		for index in missing_index:
+			f.write(f"""Universe         = vanilla
+Notification     = never
+Executable       = /bin/env
+Arguments        = "singularity exec -e -B /direct -B /star -B /afs -B /gpfs -B /sdcc/lustre02 /cvmfs/star.sdcc.bnl.gov/containers/rhic_sl7.sif {pwd}/sched{jobid}_{index}.csh"
+Output           = {pwd}/log/script_{index}.out
+Log              = /tmp/maxwoo/{jobid}.condor.log
+Initialdir       = {pwd}
+kill_sig         = SIGINT
+PeriodicRemove   = (NumJobStarts >=1 && JobStatus==1) || (JobStatus == 2 && (CurrentTime - JobCurrentStartDate > (54000)) && ((RemoteUserCpu+RemoteSysCpu)/(CurrentTime-JobCurrentStartDate) < 0.10)) || (((CurrentTime - EnteredCurrentStatus) > (2*24*3600)) && JobStatus == 5) || (JobRunCount >= 1 && JobStatus == 1)
+Priority         = +10
+Queue
+
+
+""")
+			
+	# with open('resubmit.sh', 'w') as f:
+	# 	f.write('#!/bin/bash\n')
+	# 	f.write(f'star-submit-beta -r {missing_index_str} *.session.xml')	
 
 
 if __name__ == '__main__':
