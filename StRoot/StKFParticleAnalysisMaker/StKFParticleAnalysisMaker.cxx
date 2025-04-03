a8340f62e8303e72712e0491fbf069f98abec57f
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2025-04-02 22:56:20 -0700

    result93_1
    New coal result with full EPD

--- StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
+++ StRoot/StKFParticleAnalysisMaker/StKFParticleAnalysisMaker.cxx
@@ -291,7 +291,7 @@ Int_t StKFParticleAnalysisMaker::Init()
 	PerformAnalysis = true;
 	PerformMixing = false;
 	CheckWeights2D = false;
-	UseSpectator = true; // true for v1 analysis
+	UseSpectator = false; // true for v1 analysis
 	StoringTree = false;
 	CutCent = true; // for omega, should always be true
 	PtReweighting = false;
